b51459bccef5d58a6aaa14c4b7e77849f59e9884
Alex Popelard <apopelard2014@kellogg.northwestern.edu>
2013-11-04 14:52:00 -0600

    Part I - Single picture display

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,11 @@
 class PicturesController < ApplicationController
   def show
+    if (params[:id].to_i < Picture.count + 1) && (params[:id].to_i > 1)
+      @picture = Picture.find(params[:id])
+    else
+      @picture = Picture.new
+      @picture.source = ""
+      @picture.caption = "Sorry this picture does not exist. There are only #{Picture.count} pictures to display (numbered from 1 to #{Picture.count})"
+    end
   end
 end
