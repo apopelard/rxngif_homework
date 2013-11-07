d7342a1dea3de068ee2ccccfe75322c25246d568
Alex Popelard <apopelard2014@kellogg.northwestern.edu>
2013-11-06 18:15:04 -0600

    Part IV: Edit (Show a pre-filled form)

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,12 +1,6 @@
 class PicturesController < ApplicationController
   def show
-    if (params[:id].to_i < Picture.count + 1) && (params[:id].to_i > 0)
-      @picture = Picture.find(params[:id])
-    else
-      @picture = Picture.new
-      @picture.source = ""
-      @picture.caption = "Sorry this picture does not exist. There are only #{Picture.count} pictures to display (numbered from 1 to #{Picture.count})"
-    end
+    @picture = Picture.find(params[:id])
   end
 
   def index
@@ -25,6 +19,10 @@ class PicturesController < ApplicationController
 
   def destroy
     Picture.find(params[:id]).destroy
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
   end
 
 end
